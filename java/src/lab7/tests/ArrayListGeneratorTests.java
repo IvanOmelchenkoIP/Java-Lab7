95fec2c63986b01c9057b206af08e59e2606fe05
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-21 17:54:04 +0200

    Implement unit tests for generator
    Implement unit tests for prime generator

--- java/src/lab7/tests/ArrayListGeneratorTests.java
+++ java/src/lab7/tests/ArrayListGeneratorTests.java
@@ -2,13 +2,55 @@ package lab7.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
+import lab7.generator.*;
 
 class ArrayListGeneratorTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TestGenerateSmallerThanMiniman() {
+		ArrayList<Integer> expected = new ArrayList<Integer>();
+
+		ArrayListGenerator<Integer> generator = new ArrayListGenerator<Integer>();
+		ArrayList<Integer> list = generator.generate(ArrayListGeneratorRules.PRIMES_ERATOSTHENES_SIEVE, -2);
+
+		assertTrue(expected.equals(list));
 	}
 
+	@Test
+	void TestGenerateSingle() {
+		Integer[] init = {2}; 
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(init));
+		
+		ArrayListGenerator<Integer> generator = new ArrayListGenerator<Integer>();
+		ArrayList<Integer> list = generator.generate(ArrayListGeneratorRules.PRIMES_ERATOSTHENES_SIEVE, 2);
+		
+		assertTrue(expected.equals(list));
+	}
+
+	@Test
+	void TestGenerateSeveralMaxPrime() {
+		Integer[] init = {2, 3, 5, 7}; 
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(init));
+
+		ArrayListGenerator<Integer> generator = new ArrayListGenerator<Integer>();
+		ArrayList<Integer> list = generator.generate(ArrayListGeneratorRules.PRIMES_ERATOSTHENES_SIEVE, 7);
+
+		assertTrue(expected.equals(list));
+	}
+	
+	@Test
+	void TestGenerateSeveralMaxNonPrime() {
+		Integer[] init = {2, 3, 5, 7, 11}; 
+		ArrayList<Integer> expected = new ArrayList<Integer>(Arrays.asList(init));
+
+		ArrayListGenerator<Integer> generator = new ArrayListGenerator<Integer>();
+		ArrayList<Integer> list = generator.generate(ArrayListGeneratorRules.PRIMES_ERATOSTHENES_SIEVE, 12);
+
+		assertTrue(expected.equals(list));
+	}
 }
