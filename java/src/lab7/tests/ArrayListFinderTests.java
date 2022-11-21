59e76c396c638e376578aa636ac9c61bf8f7b8e2
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-21 18:12:03 +0200

    Implement finder tests
    Implement most binary zeroes finder unit tests

--- java/src/lab7/tests/ArrayListFinderTests.java
+++ java/src/lab7/tests/ArrayListFinderTests.java
@@ -2,13 +2,47 @@ package lab7.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
+import lab7.finder.*;
 
 class ArrayListFinderTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TestFindPrimeEmpty() {
+		int expected = 0;
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		ArrayListFinder<Integer> finder = new ArrayListFinder<Integer>();
+		int result = finder.find(ArrayListFinderRules.MOST_BINARY_NUMBERS, list);
+
+		assertEquals(expected, result);
 	}
 
+	@Test
+	void TestFindPrimeSingle() {
+		int expected = 2;
+
+		Integer[] init = { 2 };
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(init));
+		ArrayListFinder<Integer> finder = new ArrayListFinder<Integer>();
+		int result = finder.find(ArrayListFinderRules.MOST_BINARY_NUMBERS, list);
+
+		assertEquals(expected, result);
+	}
+	
+	@Test
+	void TestFindPrimeMultiple() {
+		int expected = 17;
+
+		Integer[] init = { 2, 3, 5, 11, 13, 17, 19 };
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(init));
+		ArrayListFinder<Integer> finder = new ArrayListFinder<Integer>();
+		int result = finder.find(ArrayListFinderRules.MOST_BINARY_NUMBERS, list);
+
+		assertEquals(expected, result);
+	}
 }
