5b08a437e59c0ca2758312f166327ea71be4c3d1
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-21 17:42:00 +0200

    Add classes for tests
    Add classes for further implementation of unit tests

---
+++ java/src/lab7/tests/ArrayListFinderTests.java
@@ -0,0 +1,14 @@
+package lab7.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayListFinderTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
