f9e8c632f3cc1b5eb057e080baf17eb6bf981770
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-21 18:06:45 +0200

    Implement tests for convertors
    Implement unit tests for toString convertors

--- java/src/lab7/tests/ToStringConvertorTests.java
+++ java/src/lab7/tests/ToStringConvertorTests.java
@@ -2,13 +2,83 @@ package lab7.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
+import lab7.convertor.ToStringConvertor;
+import lab7.convertor.ToStringConvertorRules;
 
 class ToStringConvertorTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TestToStringEmpty() {
+		String expected = "[]";
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		ToStringConvertor<ArrayList<Integer>> convertor = new ToStringConvertor<ArrayList<Integer>>();
+		String result = convertor.convert(ToStringConvertorRules.INT_ARR_LIST_TO_STRING, list);
+
+		assertEquals(expected, result);
 	}
 
+	@Test
+	void TestToStringSingle() {
+		String expected = "[2]";
+
+		Integer[] init = { 2 };
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(init));
+		ToStringConvertor<ArrayList<Integer>> convertor = new ToStringConvertor<ArrayList<Integer>>();
+		String result = convertor.convert(ToStringConvertorRules.INT_ARR_LIST_TO_STRING, list);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	void TestToStringMultiple() {
+		String expected = "[2, 3, 5, 7, 11]";
+
+		Integer[] init = { 2, 3, 5, 7, 11 };
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(init));
+		ToStringConvertor<ArrayList<Integer>> convertor = new ToStringConvertor<ArrayList<Integer>>();
+		String result = convertor.convert(ToStringConvertorRules.INT_ARR_LIST_TO_STRING, list);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	void TestToBinaryStringEmpty() {
+		String expected = "[]";
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		ToStringConvertor<ArrayList<Integer>> convertor = new ToStringConvertor<ArrayList<Integer>>();
+		String result = convertor.convert(ToStringConvertorRules.INT_ARR_LIST_TO_BINARY_STRING, list);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	void TestToBinaryStringSingle() {
+		String expected = "[10]";
+
+		Integer[] init = { 2 };
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(init));
+		ToStringConvertor<ArrayList<Integer>> convertor = new ToStringConvertor<ArrayList<Integer>>();
+		String result = convertor.convert(ToStringConvertorRules.INT_ARR_LIST_TO_BINARY_STRING, list);
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	void TestToBinaryStringMultiple() {
+		String expected = "[10, 11, 101, 111, 1011]";
+
+		Integer[] init = { 2, 3, 5, 7, 11 };
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(init));
+		ToStringConvertor<ArrayList<Integer>> convertor = new ToStringConvertor<ArrayList<Integer>>();
+		String result = convertor.convert(ToStringConvertorRules.INT_ARR_LIST_TO_BINARY_STRING, list);
+
+		assertEquals(expected, result);
+	}
 }
